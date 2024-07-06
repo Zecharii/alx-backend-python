3f277918d810a31fa2f595a60052d5e9dc0d966b
Zechariah Gaiya <abbazechariah@gmail.com>
2024-07-06 07:26:54 +0000

    Adding changes to file 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
-""
-Type-annotated function add.
+""Annotation for add function.
 """
 
 
 def add(a: float, b: float) -> float:
     """Adds two floats together and returns the sum.
-
-    Args:
-        a: The first float number.
-        b: The second float number.
-
-    Returns:
-        The sum of a and b as a float.
     """
-
     return a + b
